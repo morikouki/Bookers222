e49b094edea8a63193e4d6a1112823a42be94bc3
vagrant <akzouksia@icloud.com>
2020-08-25 07:48:15 +0000

    [Finish]ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,11 @@ class FavoritesController < ApplicationController
 	def create
 		favorite = current_user.favorites.new(book_id: @book.id)
 		favorite.save
-		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		favorite = current_user.favorites.find_by(book_id: @book.id)
 		favorite.destroy
-		redirect_back(fallback_location: root_path)
 	end
 
 	private
